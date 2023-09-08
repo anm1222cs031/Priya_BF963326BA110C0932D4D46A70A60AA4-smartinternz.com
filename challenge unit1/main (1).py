9a70466a31203a3aa81921591c9d81af4cb63d25
anm1222cs031 <144424058+anm1222cs031@users.noreply.github.com>
2023-09-08 15:03:17 +0530

    Add files via upload

---
+++ challenge unit1/main (1).py
@@ -0,0 +1,15 @@
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Taking input from the user
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    result = factorial(num)
+    print(f"The factorial of {num} is {result}")
