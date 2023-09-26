a2f4c7086145f4c1c3f9649a87efd1ece9646e15
anm1222cs031 <144424058+anm1222cs031@users.noreply.github.com>
2023-09-26 14:37:36 +0530

    Update and rename main2.py.py to main2.py

--- challaenge unit 3/main2.py
+++ challaenge unit 3/main2.py
@@ -8,7 +8,7 @@ def sort_students(student_list):
     sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
     return sorted_students
 students1 = [
-    Student("bavithra", "S101", 6.8),
+    Student("bavani", "S101", 6.8),
     Student("arthi", "D102", 5.6),
     Student("Gobi", "G103", 4.4),
     Student("gobika", "B104", 3.3),
